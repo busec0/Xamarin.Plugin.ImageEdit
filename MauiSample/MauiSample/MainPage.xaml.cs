c0dbd5ecbb6153c7098442ce1d9596a9ff0edeb0
Bulzan Sergiu <bulzan.sergiu@gmail.com>
2022-06-29 12:36:20 +0300

    Add MAUI Sample app

---
+++ MauiSample/MauiSample/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace MauiSample;
+
+public partial class MainPage : ContentPage
+{
+    private ImageSource _image;
+    public ImageSource Image
+    {
+        get => _image;
+        set
+        {
+            _image = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private FileResult _imageFile;
+
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    async void Pick_Clicked(System.Object sender, System.EventArgs e)
+    {
+        _imageFile = await Microsoft.Maui.Media.MediaPicker.PickPhotoAsync();
+        var stream = await _imageFile.OpenReadAsync();
+        Image = ImageSource.FromStream(() => stream);
+    }
+
+    async void ToMonochrome_Clicked(System.Object sender, System.EventArgs e)
+    {
+        var stream = await _imageFile.OpenReadAsync();
+
+        using (var image = await Plugin.ImageEdit.CrossImageEdit.Current.CreateImageAsync(stream))
+        {
+            var data = image.ToMonochrome().ToJpeg(100);
+            Image = ImageSource.FromStream(() => new MemoryStream(data));
+        }
+    }
+
+    async void Resize_Clicked(System.Object sender, System.EventArgs e)
+    {
+        var stream = await _imageFile.OpenReadAsync();
+
+        using (var image = await Plugin.ImageEdit.CrossImageEdit.Current.CreateImageAsync(stream))
+        {
+            var data = image.Resize(150).ToJpeg(100);
+            Image = ImageSource.FromStream(() => new MemoryStream(data));
+        }
+    }
+}
+
+
