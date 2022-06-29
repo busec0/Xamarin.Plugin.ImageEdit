72dd03fac773f57c1dea01e331f1e67935730e59
Bulzan Sergiu <bulzan.sergiu@gmail.com>
2022-06-29 11:42:42 +0300

    update nuget pack details

--- nuget/pack.sh
+++ nuget/pack.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mono --runtime=v4.0 nuget.exe pack ImageEdit.Plugin.nuspec -symbols -Prop Configuration=Release -verbosity detailed -basepath ./ -OutputDirectory ~/projects/nuget
+mono nuget.exe pack ImageEdit.Plugin.nuspec -verbosity detailed -basepath ./ -OutputDirectory ./dist
